fb8d33b2c478521764e19936c6fb7267a501f40e
Panaria <baranyldzby@gmail.com>
2023-10-16 20:41:42 +0300

    Add project files.

---
+++ kelimeyi ters yazma/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kelimeyi_ters_yazma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kelime = textBox1.Text;
+
+            for (int i = kelime.Length - 1; i > -1; i--)
+            {
+                textBox2.Text = textBox2.Text + textBox1.Text[i];
+            }
+            textBox1.Clear();
+        }
+    }
+}
